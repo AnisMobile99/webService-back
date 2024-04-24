0bd750befd4b59f253e9dc1d3199fb94ecf29bed
AnisMobile99 <anis.salahbey.dz@gmail.com>
2024-04-24 17:49:31 +0200

    [fixed]putFilm function done

--- services/putManager.js
+++ services/putManager.js
@@ -1,10 +1,18 @@
 const updateFilm = (uid, updatedFilm) => {
+	// Trouver l'index du film basé sur son id
 	const index = films.findIndex((film) => film.id === parseInt(uid));
+
 	if (index !== -1) {
+		// Mise à jour du film avec les nouvelles données en s'assurant de ne pas écraser l'id
 		films[index] = { ...films[index], ...updatedFilm };
+
+		// Retourner le film mis à jour pour confirmation
+		return films[index];
+	} else {
+		// Retourner null si aucun film n'est trouvé avec l'id fourni
+		return null;
 	}
 };
-
 module.exports = {
 	updateFilm,
 };
